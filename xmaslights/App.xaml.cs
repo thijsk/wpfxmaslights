5c555ed04c523c6308ed4610f9dbe02ccda1f947
Thijs Kroesbergen <thijsk@users.noreply.github.com>
2008-12-08 10:55:44 +0000

    initial version

---
+++ xmaslights/App.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Windows;
+using System.Windows.Threading;
+
+namespace xmaslights
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+
+        private static readonly InterceptKeys.LowLevelKeyboardProc _proc = HookCallback;
+        private static IntPtr _hookID = IntPtr.Zero;
+        private delegate void KeyHit();
+        private static KeyHit _keyHit;
+
+        public App()
+        {
+            ShutdownMode = ShutdownMode.OnExplicitShutdown;
+            try
+            {
+                _hookID = InterceptKeys.SetHook(_proc);
+            }
+            catch
+            {
+                if (_hookID != IntPtr.Zero)
+                    InterceptKeys.UnhookWindowsHookEx(_hookID);
+            }
+        }
+
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            Controller c = new Controller();
+            _keyHit = c.KeyHit;
+            c.Launch();
+        }
+
+        private static int _keyCount;
+        public static IntPtr HookCallback(int nCode, IntPtr wParam, IntPtr lParam)
+        {
+            if (_keyHit != null && ++_keyCount == 10)
+            {
+                _keyCount = 0;
+                Dispatcher.CurrentDispatcher.BeginInvoke(_keyHit, DispatcherPriority.Input);
+            }
+            return InterceptKeys.CallNextHookEx(_hookID, nCode, wParam, lParam);
+        }
+
+    }
+
+}
---
+++ xmaslights/BackWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Runtime.InteropServices;
+using System.Windows.Interop;
+
+
+namespace xmaslights
+{
+    /// <summary>
+    /// Interaction logic for BackWindow.xaml
+    /// </summary>
+    public partial class BackWindow : Window
+    {
+        private readonly Controller controller;
+        public Controller Controller { get { return controller; } }
+
+        public BackWindow(Controller c)
+        {
+            this.controller = c;
+            this.DataContext = controller;
+            InitializeComponent();
+        }
+
+        public const int WS_EX_TRANSPARENT = 0x00000020;
+        public const int GWL_EXSTYLE = (-20);
+
+        [DllImport("user32.dll")]
+        public static extern int GetWindowLong(IntPtr hwnd, int index);
+
+        [DllImport("user32.dll")]
+        public static extern int SetWindowLong(IntPtr hwnd, int index, int newStyle);
+
+
+        protected override void  OnSourceInitialized(EventArgs e)
+        {
+ 	        base.OnSourceInitialized(e);
+
+            // Get this window's handle
+            IntPtr hwnd = new WindowInteropHelper(this).Handle;
+
+            // Change the extended window style to include WS_EX_TRANSPARENT
+            int extendedStyle = GetWindowLong(hwnd, GWL_EXSTYLE);
+            SetWindowLong(hwnd, GWL_EXSTYLE, extendedStyle | WS_EX_TRANSPARENT);
+        }
+    }
+
+    
+}
